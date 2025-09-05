01d73156a83033626b24af18792fecbcbb2ba9d8
João Paulo Barbosa <joao.31582129@gmail.com>
2025-09-05 14:18:58 -0300

    finish

--- R/load_births.R
+++ R/load_births.R
@@ -4,7 +4,7 @@
 #' part of DATASUS, used in public health analyses.
 #'
 #' @param time_period A numeric value or vector indicating the year(s) of the data to be downloaded. For example, `2020` or `2015:2020`.
-#' @param states A string or vector of strings indicating the Brazilian state(s) for which the data should be downloaded. Use `"all"` to download data for the entire country. For specific states, use abbreviations like `"SP"`, `"RJ"`, or `c("SP", "RJ")`.
+#' @param states A string or vector of strings indicating the Brazilian state(s) for which the data should be downloaded. Use `"all"` (by default) to download data for the entire country. For specific states, use abbreviations like `"SP"`, `"RJ"`, or `c("SP", "RJ")`.
 #' @param raw_data Logical. If `TRUE`, returns the raw data exactly as provided by DATASUS. If `FALSE` (default), returns a cleaned and standardized version of the dataset.
 #' @param language A string indicating the desired language of variable names and labels. Accepts `"eng"` (default) for English or `"pt"` for Portuguese.
 #'
