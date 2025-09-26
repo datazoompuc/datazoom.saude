0c7a3e4aace12e40cff232be2627c7dd2dc77117
Laura T. Regadas <113269042+lauratregadas@users.noreply.github.com>
2025-09-26 17:13:10 -0300

    correct year

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Please cite as: ")
-  packageStartupMessage("Data Zoom (2025). Data Zoom: Simplifying Access To Brazilian Microdata.
+  packageStartupMessage("Data Zoom (2023). Data Zoom: Simplifying Access To Brazilian Microdata.
 https://www.econ.puc-rio.br/datazoom/english/index.html")
 
   packageStartupMessage("\nOr if you prefer a BibTeX entry: ")
@@ -8,6 +8,6 @@ https://www.econ.puc-rio.br/datazoom/english/index.html")
 	     author = {Data Zoom},
 	     title = {Data Zoom: Simplifying Access To Brazilian Microdata},
 	     url = {https://www.econ.puc-rio.br/datazoom/english/index.html},
-	     year = {2025},
+	     year = {2023},
 }")
 }
