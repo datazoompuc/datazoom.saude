295218dc928b82f771907840e3493366843ff6ba
LuanaLaport <luanalaportsantos@icloud.com>
2026-06-28 16:55:37 -0300

    Update outdated econ.puc-rio.br URL to datazoom.com.br/en/dz_saude

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,13 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Please cite as: ")
   packageStartupMessage("Data Zoom (2023). Data Zoom: Simplifying Access To Brazilian Microdata.
-https://www.econ.puc-rio.br/datazoom/english/index.html")
+https://datazoom.com.br/en/dz_saude")
 
   packageStartupMessage("\nOr if you prefer a BibTeX entry: ")
   packageStartupMessage("@Unpublished{DataZoom2023,
 	     author = {Data Zoom},
 	     title = {Data Zoom: Simplifying Access To Brazilian Microdata},
-	     url = {https://www.econ.puc-rio.br/datazoom/english/index.html},
+	     url = {https://datazoom.com.br/en/dz_saude},
 	     year = {2023},
 }")
 }
