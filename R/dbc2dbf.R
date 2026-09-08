a2b7e50c0c3a300d5e026d752ede3b4c7fedcf82
LuanaLaport <luanalaportsantos@icloud.com>
2026-09-08 10:20:45 -0300

    ultimas alteracoes submissoa cran

--- R/dbc2dbf.R
+++ R/dbc2dbf.R
@@ -31,16 +31,10 @@
 #' @seealso \code{read.dbc}
 #' @examples
 #' \donttest{
-#' # Input file name
 #' in.f <- system.file("files/sids.dbc", package = "datazoom.saude")
-#'
-#' # Output file name
-#' out.f <- tempfile(fileext = ".dbc")
-#'
-#' # The call return logi = TRUE on success
-#' if (dbc2dbf(input.file = in.f, output.file = out.f)) {
-#'   print("File decompressed!")
-#'   file.remove(out.f)
+#' out.f <- tempfile(fileext = ".dbf")
+#' if (file.exists(in.f)) {
+#'   dbc2dbf_wrapper(input.file = in.f, output.file = out.f)
 #' }
 #' }
 #'
